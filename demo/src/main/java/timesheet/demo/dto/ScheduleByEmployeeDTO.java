40130df22a3b478b7934efafeb47c3cefce4714b
goodguyfr <trieuduong0806@gmail.com>
2024-03-12 20:15:19 +0700

    update commit

---
+++ demo/src/main/java/timesheet/demo/dto/ScheduleByEmployeeDTO.java
@@ -0,0 +1,12 @@
+package timesheet.demo.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ScheduleByEmployeeDTO {
+    private String userId;
+    List<CreateScheduleByEmployeeDTO> createPlan;
+//    List<CheckScheduleByManagerDTO> checkByManager;
+}
